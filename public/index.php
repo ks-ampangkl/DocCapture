bf4d0c475f5e5c8feaefe9589d81a7ff33eeee10
ks-ampangkl <kugaann@graduate.utm.my>
2026-08-19 19:16:07 +0800

    Component 1 Commit

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+
+$app = AppFactory::create();
+$app->add(new App\Middleware\JsonBodyParser());
+$app->add(new App\Middleware\Cors());
+$app->addRoutingMiddleware();
+$app->addErrorMiddleware(true, true, true);
+
+(require __DIR__ . '/../src/routes.php')($app);
+$app->run();
